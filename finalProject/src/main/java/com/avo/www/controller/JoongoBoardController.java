22fc05c01edcd8cdb2b8718988136c524e576021
flosian <giy0919@gmail.com>
2023-11-16 17:18:59 +0900

    first commit

--- finalProject/src/main/java/com/avo/www/controller/JoongoBoardController.java
+++ finalProject/src/main/java/com/avo/www/controller/JoongoBoardController.java
@@ -1,10 +1,16 @@
 package com.avo.www.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.avo.www.domain.ProductBoardVO;
 import com.avo.www.service.JoongoBoardService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +20,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/joongo/**")
 public class JoongoBoardController {
 
+	@Inject
 	private JoongoBoardService jbsv;
 	
 	@GetMapping("/list")
-	public void list() {
+	public void list(Model m) {
+		List<ProductBoardVO> list = jbsv.getList();
+		m.addAttribute("list",list);
 	}
 	
 	@GetMapping("/register")
@@ -25,7 +34,10 @@ public class JoongoBoardController {
 	}
 	
 	@PostMapping("/register")
-	public String postReg() {
+	public String postReg(ProductBoardVO pbvo) {
+		log.info(">>>>>>> pbvo >>> "+pbvo);
+		int isOk = jbsv.register(pbvo);
+		log.info("등록 "+(isOk > 0 ? "성공" : "실패"));
 		
 		return "redirect:/joongo/list";
 	}
