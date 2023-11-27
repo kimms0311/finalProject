c1da20119ae4885cc6703a1d990776fbd9a02f98
hayxxnekim <hayxxne@gmail.com>
2023-11-27 21:00:29 +0900

    ignore

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/finalProject/resources/js/storeBoardDetail.js
+++
@@ -1,9 +0,0 @@
-//확인 클릭 시 게시글 삭제
-document.addEventListener('click', (e) => {
-    if(e.target.id == 'delBtn') {
-       if(!confirm("게시글을 삭제하시겠습니까?")) {
-            e.preventDefault();
-            return;
-       }
-    }
-})
