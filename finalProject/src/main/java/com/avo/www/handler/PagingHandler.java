c07bee99c4b07df6e7930905b25a007aa8e49874
hayxxnekim <hayxxne@gmail.com>
2023-11-27 19:26:51 +0900

    1127

--- finalProject/src/main/java/com/avo/www/handler/PagingHandler.java
+++ finalProject/src/main/java/com/avo/www/handler/PagingHandler.java
@@ -2,21 +2,20 @@ package com.avo.www.handler;
 
 import java.util.List;
 
+
+import com.avo.www.domain.CommunityCmtVO;
+import com.avo.www.domain.CommunityReCmtVO;
 import com.avo.www.domain.FileVO;
 import com.avo.www.domain.PagingVO;
 import com.avo.www.domain.ProductBoardVO;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
-@AllArgsConstructor
 public class PagingHandler {
 
    private int startPage;
@@ -27,37 +26,50 @@ public class PagingHandler {
    private PagingVO pgvo;
    
    private List<ProductBoardVO> prodList;
+   private List<CommunityCmtVO> cmtList;
+   private List<CommunityReCmtVO> reCmtList;
    
    //1123 하연 추가
    private List<FileVO> prodFileList;
+
+
    
-   public PagingHandler(PagingVO pgvo, int totalCount) {
+   public PagingHandler(PagingVO pgvo, int totalCount, int minus) {
       
       this.pgvo = pgvo;
       this.totalCount = totalCount;
       
       this.endPage = 
-				(int)Math.ceil(pgvo.getPageNo() / (double)pgvo.getQty())*pgvo.getQty();
-		
-		this.startPage = endPage - 9;		
-		this.realEndPage = 
-				(int)Math.ceil(totalCount/(double)pgvo.getQty());
-		
-		if(realEndPage < endPage) {
-			this.endPage = realEndPage;
-		}
+            (int)Math.ceil(pgvo.getPageNo() / (double)pgvo.getQty()) * pgvo.getQty();
+     this.startPage = endPage - minus;
+     
+     this.realEndPage = (int)Math.ceil(totalCount / (double)pgvo.getQty());
+     if(this.realEndPage < this.endPage) {
+         this.endPage = this.realEndPage;
+      }
+     
+     
    }
    
+   // 게시글 페이징용
    public PagingHandler(PagingVO pgvo, int totalCount, List<ProductBoardVO> prodList) {
-      this(pgvo, totalCount);
+      this(pgvo, totalCount, 7);
       this.prodList = prodList;
+   }
+   
+   //댓글 페이징용
+   public PagingHandler(int totalCount, PagingVO pgvo, List<CommunityCmtVO> cmtList, List<CommunityReCmtVO> reCmtList) {
+      this(pgvo, totalCount, 9);
+      this.cmtList = cmtList;
+      this.reCmtList = reCmtList;
    }
    
    //1123 하연 추가
    public PagingHandler(PagingVO pgvo, int totalCount, List<ProductBoardVO> prodList, List<FileVO> prodFileList) {
-	      this(pgvo, totalCount);
-	      this.prodList = prodList;
-	      this.prodFileList =  prodFileList;
-	   }
-   
+         this(pgvo, totalCount, 9);
+         this.prodList = prodList;
+         this.prodFileList =  prodFileList;
+      }
+
+
 }
