4cf510620b7bed7b0cbb1b0b5c9948c163d1fee7
kimms0311 <ahd070721@gmail.com>
2023-12-06 21:43:27 +0900

    231206 캐시삭제

--- finalProject/src/main/java/com/avo/www/config/SecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.avo.www.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-//AbstractSecurityWebApplicationInitializer 상속받아야
-//security 관련 필터들이 활성화된다.
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
